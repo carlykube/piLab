e3ba2e38bf4f3fdad6049af7396eb85d1677d3c2
ckubacak <ckubacak@smu.edu>
2014-02-21 15:05:03 -0600

    fix setup.php

--- php/setup.php
+++ php/setup.php
@@ -1,12 +1,12 @@
 <?php
 	function autoloader($class) {
-		include_once '/classes/'.$class.'.class.php';
+		include_once '/php/classes/'.$class.'.class.php';
 	}
 	spl_autoload_register('autoloader');
 
-	include_once "./translations/".basename($_SERVER['SCRIPT_NAME'],'.php');
+	include_once "/translations/".basename($_SERVER['SCRIPT_NAME'],'.php');
 	$Translator = new Translator("en"); //'en' for English; 'es' for Spanish
 
-	require "./libs/Smarty.class.php";
+	require "/libs/Smarty.class.php";
 	$smarty = new Smarty;
 ?>
