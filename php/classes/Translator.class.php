4ae585fceeffc085d6b185e72c29eb9d2c67d97f
conradhappeliv <conrad.h.appel.iv@gmail.com>
2014-03-04 22:37:04 -0600

    New cleaner translation system

--- php/classes/Translator.class.php
+++ php/classes/Translator.class.php
@@ -1,16 +1,23 @@
 <?php
 	class Translator {
 		private $language;
-			function __construct($language) {
-				if($language == "en") {
-					$this->language = 0;
-				} elseif($language == "es") {
-					$this->language = 1;
-				}
+		function __construct($language) {
+			if($language == "en") {
+				$this->language = 0;
+			} elseif($language == "es") {
+				$this->language = 1;
 			}
-			
-			function getText($textName) {
-				return $GLOBALS[$textName][$this->language];
+		}
+		
+		function getText($textName) {
+			return $GLOBALS['translations'][$textName][$this->language];
+		}
+		
+		function assignAllVariables() {
+			$smarty = &$GLOBALS['smarty'];
+			foreach($GLOBALS['translations'] as $k => $v) {
+				$smarty->assign($k,$v[$this->language]);
 			}
+		}
 	}
 ?>
