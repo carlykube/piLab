1501c8837dc7ab1accd053c9be4bc9cdfbc59e39
conradhappeliv <conrad.h.appel.iv@gmail.com>
2014-03-04 21:49:41 -0600

    Added registration type page

--- register.php
+++ register.php
@@ -2,16 +2,23 @@
 
 require "php/setup.php";
 
-$smarty->assign("firstname",$Translator->getText("firstname"));
-$smarty->assign("lastname", $Translator->getText("lastname"));
-$smarty->assign("username",$Translator->getText("username"));
-$smarty->assign("password", $Translator->getText("password"));
-$smarty->assign("gender",$Translator->getText("gender"));
-$smarty->assign("birthday",$Translator->getText("birthday"));
-$smarty->assign("hometown", $Translator->getText("hometown"));
-$smarty->assign("submit", $Translator->getText("submit"));
-$smarty->assign("firstlastuser", $Translator->getText("firstlastuser"));
+if(isset($_GET['registrationtype'])) {
+	$smarty->assign("registrationtype",$_GET['registrationtype']);
+	
+	$smarty->assign("firstname",$Translator->getText("firstname"));
+	$smarty->assign("lastname", $Translator->getText("lastname"));
+	$smarty->assign("username",$Translator->getText("username"));
+	$smarty->assign("password", $Translator->getText("password"));
+	$smarty->assign("gender",$Translator->getText("gender"));
+	$smarty->assign("birthday",$Translator->getText("birthday"));
+	$smarty->assign("hometown", $Translator->getText("hometown"));
+	$smarty->assign("submit", $Translator->getText("submit"));
+	$smarty->assign("firstlastuser", $Translator->getText("firstlastuser"));
 
-$smarty->display('register.tpl');
+	$smarty->display('register.tpl');
+} else {
+	$smarty->assign("whoareyou", $Translator->getText('whoareyou'));
+	$smarty->display('registrationtype.tpl');
+}
 
 ?>
---
+++ templates/registrationtype.tpl
@@ -0,0 +1,10 @@
+{include file="header.tpl" title=Register}
+	
+	<div id="registrationtypes">
+		<h3>{$whoareyou}</h3>
+		<div><a href="?registrationtype=teacher">Teacher</a></div>
+		<div><a href="?registrationtype=mentor">Mentor</a></div>
+		<div><a href="?registrationtype=student">Student</a></div>
+	</div>
+	
+{include file="footer.tpl"}
