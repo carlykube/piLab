ed496f87cea2d0fe36968ce2b6448a57e93c3008
EmVilleda <emvilledaprincipe@yahoo.com>
2014-05-02 14:55:15 -0500

    More Changes - Contact

--- contacts.php
+++ contacts.php
@@ -2,6 +2,13 @@
 
 require "php/setup.php";
 
+if ($acct->logged){
+
+	$smarty->assign('unreadLetters', $unreadLetters);
+	$smarty->assign('name', $user->getName());
+	$smarty->assign('contacts', $user->getContacts());	
+}
+
 $Translator->assignAllVariables();
 
 $smarty->display('contacts.tpl');
